ffe14abdd2ee20a43dee58a3ea83dc714e644f56
Solomon Liu <sliu198@gmail.com>
2017-02-14 03:27:12 -0800

    nope. github CORS blocks getting swagger.json

--- src/ts/main/kidaptive_constants.ts
+++ src/ts/main/kidaptive_constants.ts
@@ -3,7 +3,7 @@
  */
 
 class KidaptiveConstants {
-    static SWAGGER_URL = "https://github.com/Kidapt/kidaptive-sdk-js/raw/cookies/dist/swagger.json";
+    static SWAGGER_URL = "http://localhost:63342/kidaptive-sdk-js/dist/swagger.json";
     static SWAGGER_HOST = "develop.kidaptive.com";
     static SWAGGER_SCHEMES = ["https"];
 
