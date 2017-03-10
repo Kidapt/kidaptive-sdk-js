7f87bbe1a0f286e86751b3606a0715d75a7733b3
Solomon Liu <sliu198@gmail.com>
2017-03-09 16:41:40 -0800

    ALP-1208 MAP estimation with guessing parameter using log likelihoods.
    ALP-1208 add IRT guessing parameter SD estimation. Update test cases for IRT
    
    ALP-1208 update test script
    
    ALP-1208 split test data into multiple files

--- src/js/main/irt/irt.h
+++ src/js/main/irt/irt.h
@@ -14,6 +14,6 @@
 double normal_dist(double x, double mu, double sigma);
 double logistic_dist(double x, double mu, double alpha);
 double inv_logis(double p);
-void estimate (bool outcome, double difficulty, double prior_mean, double prior_sd, double *post_mean, double *post_sd);
+void estimate (double outcome, double difficulty, double guessing, double prior_mean, double prior_sd, double *post_mean, double *post_sd);
 
 #endif /* kidaptive_irt_h */
