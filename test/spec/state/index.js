ac69d89f73e5672a99e13daa080bd8adfba9ba7f
cameronperry <cameron.perry@kidaptive.com>
2018-09-05 16:24:58 -0700

    Add State unit tests for copy=true/false

--- test/spec/state/index.js
+++ test/spec/state/index.js
@@ -1,14 +1,23 @@
 'use strict';
 import TestUtils from '../test-utils';
 import State from '../../../src/state';
+import Utils from '../../../src/utils';
 import Should from 'should';
 
 export default () => {
 
   describe('State', () => {
+    let copiedObject;
+    const testObject = {
+      childArray: [1, {subProp: 'sub array value'}],
+      childNumber: 200,
+      childObject: {subProp: 'sub object value'},
+      childString: 'string value'
+    }
 
     beforeEach(() => {
       TestUtils.resetStateAndCache();
+      copiedObject = Utils.copyObject(testObject);
     });
 
     after(() => {
@@ -61,6 +70,56 @@ export default () => {
       Should(State.get('testKey1')).equal(undefined);
       Should(State.get('testKey2')).equal(undefined);
     });
+
+    describe('Copy True (default)', () => {
+
+      it('State should stay constant when updating object passed to State.set', () => {
+        State.set('testObject', copiedObject);
+        copiedObject.childString = 'new string value';
+        copiedObject.childObject.subProp = 'new sub object value';
+        copiedObject.childArray[1].subProp = 'new sub array value';
+        const stateObject = State.get('testObject');
+        Should(stateObject).not.deepEqual(copiedObject);
+        Should(stateObject).deepEqual(testObject);
+      });
+
+      it('State should stay constant when updating object passed from State.get', () => {
+        State.set('testObject', copiedObject);
+        const getObject = State.get('testObject');
+        getObject.childString = 'new string value';
+        getObject.childObject.subProp = 'new sub object value';
+        getObject.childArray[1].subProp = 'new sub array value';
+        const stateObject = State.get('testObject');
+        Should(stateObject).not.deepEqual(getObject);
+        Should(stateObject).deepEqual(testObject);
+      });
+
+    }); //END Copy True (default)
+
+    describe('Copy False', () => {
+
+      it('State should not stay constant when updating object passed to State.set with copy=false', () => {
+        State.set('testObject', copiedObject, false);
+        copiedObject.childString = 'new string value';
+        copiedObject.childObject.subProp = 'new sub object value';
+        copiedObject.childArray[1].subProp = 'new sub array value';
+        const stateObject = State.get('testObject');
+        Should(stateObject).deepEqual(copiedObject);
+        Should(stateObject).not.deepEqual(testObject);
+      });
+
+      it('State should not stay constants when updating object passed from State.get with copy=false', () => {
+        State.set('testObject', copiedObject);
+        const getObject = State.get('testObject', false);
+        getObject.childString = 'new string value';
+        getObject.childObject.subProp = 'new sub object value';
+        getObject.childArray[1].subProp = 'new sub array value';
+        const stateObject = State.get('testObject');
+        Should(stateObject).deepEqual(getObject);
+        Should(stateObject).not.deepEqual(testObject);
+      });
+
+    }); //END Copy False
 
   }); //END State
 
