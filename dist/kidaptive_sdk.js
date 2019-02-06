b225852a23748b157c6dcd034ad5b138bd02010e
Solomon Liu <sliu198@gmail.com>
2019-02-05 19:03:30 -0800

    0.2.23

--- dist/kidaptive_sdk.js
+++ dist/kidaptive_sdk.js
@@ -9,159 +9,81 @@
     }
 })(typeof window !== "undefined" ? window : this, function(KidaptiveSdkGlobal) {
     "use strict";
-    var irt_irt, jquery, kidaptive_error, kidaptive_utils, kidaptive_attempt_processor, kidaptive_constants, kidaptive_event_manager, kidaptive_http_client, kidaptive_learner_manager, kidaptive_model_manager, kidaptive_recommendation_manager, kidaptive_trial_manager, kidaptive_user_manager, kidaptive_sdk;
-    irt_irt = function() {
-        var KidaptiveIrt = {};
-        (function() {
-            var normal_dist = function(x, mu, sigma) {
-                return Math.exp(-Math.pow(x - mu, 2) / 2 / Math.pow(sigma, 2)) / sigma / Math.sqrt(2 * Math.PI);
-            };
-            var logistic_dist = function(x, mu, alpha) {
-                return 1 / (1 + Math.exp(-alpha * (x - mu)));
-            };
-            var inv_logis = function(p) {
-                return Math.log(1 / p - 1) * Math.sqrt(Math.PI / 8);
-            };
-            var estimate = function(y, beta, theta, sigma, post_mean, post_sd) {
-                if (sigma === 0) {
-                    post_mean = theta;
-                    post_sd = sigma;
-                    return {
-                        post_mean: post_mean,
-                        post_sd: post_sd
-                    };
-                }
-                var x = theta;
-                var s1 = 0;
-                var s2 = 0;
-                var x_new = theta;
-                var a = Math.sqrt(8 / Math.PI);
-                var delta = 1;
-                var max = 0;
-                var upper = Infinity;
-                var lower = -Infinity;
-                var phi = 0;
-                var g = 0;
-                var y0 = 0;
-                var y1 = 0;
-                var y1sign0 = 0;
-                var y1sign1 = 0;
-                var sd_ratio = Math.exp(-.5);
-                if (!y) {
-                    a = -a;
-                }
-                while (true) {
-                    phi = normal_dist(x, theta, sigma);
-                    g = logistic_dist(x, beta, a);
-                    y0 = phi * g;
-                    y1sign0 = a * (1 - g) - (x - theta) / Math.pow(sigma, 2);
-                    y1sign1 = -Math.pow(a, 2) * g * (1 - g) - 1 / Math.pow(sigma, 2);
-                    if (y0 > max) {
-                        max = y0;
-                    }
-                    if (Math.abs(y1sign0) < 1e-6) {
-                        break;
-                    } else if (y1sign0 > 0) {
-                        lower = x;
-                    } else if (y1sign0 < 0) {
-                        upper = x;
-                    }
-                    x_new = x - y1sign0 / y1sign1;
-                    if (x_new > lower && x_new < upper) {
-                        x = x_new;
-                        continue;
-                    }
-                    if (lower > -Infinity && upper < Infinity) {
-                        x_new = (upper + lower) / 2;
-                    } else if (lower === -Infinity) {
-                        x_new = x - delta;
-                        delta = delta * 2;
-                    } else {
-                        x_new = x + delta;
-                        delta = delta * 2;
-                    }
-                    x = x_new;
-                }
-                max = normal_dist(x, theta, sigma) * logistic_dist(x, beta, a);
-                s1 = x - sigma;
-                delta = 1;
-                lower = -Infinity;
-                upper = x;
-                while (true) {
-                    phi = normal_dist(s1, theta, sigma);
-                    g = logistic_dist(s1, beta, a);
-                    y0 = phi * g;
-                    y1 = y0 * (a * (1 - g) - (s1 - theta) / Math.pow(sigma, 2));
-                    y0 = y0 - sd_ratio * max;
-                    if (Math.abs(y0) < max * 1e-6) {
-                        break;
-                    } else if (y0 < 0) {
-                        lower = s1;
-                    } else {
-                        upper = s1;
-                    }
-                    x_new = s1 - y0 / y1;
-                    if (x_new > lower && x_new < upper) {
-                        s1 = x_new;
-                        continue;
-                    }
-                    if (lower > -Infinity && upper < Infinity) {
-                        x_new = (upper + lower) / 2;
-                    } else if (lower === -Infinity) {
-                        x_new = s1 - delta;
-                        delta = delta * 2;
-                    } else {
-                        x_new = s1 + delta;
-                        delta = delta * 2;
-                    }
-                    s1 = x_new;
-                }
-                s2 = x + sigma;
-                delta = 1;
-                lower = x;
-                upper = Infinity;
-                while (true) {
-                    phi = normal_dist(s2, theta, sigma);
-                    g = logistic_dist(s2, beta, a);
-                    y0 = phi * g;
-                    y1 = y0 * (a * (1 - g) - (s2 - theta) / Math.pow(sigma, 2));
-                    y0 = y0 - sd_ratio * max;
-                    if (Math.abs(y0) < max * 1e-6) {
-                        break;
-                    } else if (y0 > 0) {
-                        lower = s2;
-                    } else {
-                        upper = s2;
-                    }
-                    x_new = s2 - y0 / y1;
-                    if (x_new > lower && x_new < upper) {
-                        s2 = x_new;
-                        continue;
-                    }
-                    if (lower > -Infinity && upper < Infinity) {
-                        x_new = (upper + lower) / 2;
-                    } else if (lower === -Infinity) {
-                        x_new = s2 - delta;
-                        delta = delta * 2;
-                    } else {
-                        x_new = s2 + delta;
-                        delta = delta * 2;
-                    }
-                    s2 = x_new;
-                }
-                var postSigma1 = x - s1;
-                var postSigma2 = s2 - x;
-                post_mean = x + Math.sqrt(2 / Math.PI) * (postSigma2 - postSigma1);
-                post_sd = Math.sqrt((Math.pow(postSigma1, 3) + Math.pow(postSigma2, 3)) / (postSigma1 + postSigma2) - 2 * Math.pow(postSigma2 - postSigma1, 2) / Math.PI);
+    var kidaptive_irt, jquery, kidaptive_error, kidaptive_utils, kidaptive_attempt_processor, kidaptive_constants, kidaptive_event_manager, kidaptive_http_client, kidaptive_learner_manager, kidaptive_model_manager, kidaptive_recommendation_manager, kidaptive_trial_manager, kidaptive_user_manager, kidaptive_sdk;
+    kidaptive_irt = function() {
+        var KidaptiveIrt = {
+            estimate: function(y, beta, theta, sigma) {
+                return estimate(y || 0, beta || 0, 0, theta || 0, sigma || 0, 0, 0);
+            }
+        };
+        var logistic_dist = function(x, mu, alpha) {
+            return 1 / (1 + Math.exp(-alpha * (x - mu)));
+        };
+        var estimate = function(y, beta, guessing, theta, sigma, post_mean, post_sd) {
+            var a = Math.sqrt(8 / Math.PI);
+            var max_sigma = 2 / a;
+            post_mean = theta;
+            post_sd = sigma = Math.min(Math.max(sigma, 0), max_sigma);
+            if (sigma === 0) {
                 return {
                     post_mean: post_mean,
                     post_sd: post_sd
                 };
+            }
+            y = y < .5 ? 0 : 1;
+            if (guessing >= 1) {
+                return {
+                    post_mean: post_mean,
+                    post_sd: post_sd
+                };
+            } else {
+                guessing = Math.max(guessing, 0);
+            }
+            var dll;
+            var ddll;
+            var x;
+            var high = Infinity;
+            var low = -Infinity;
+            var delta = 1;
+            var p;
+            var q;
+            var P;
+            do {
+                x = post_mean;
+                p = logistic_dist(post_mean, beta, a);
+                q = 1 - p;
+                if (y === 0 || guessing === 0) {
+                    dll = a * (y - p) - (post_mean - theta) * Math.pow(sigma, -2);
+                    ddll = -Math.pow(a, 2) * p * q - Math.pow(sigma, -2);
+                } else {
+                    P = guessing + (1 - guessing) * p;
+                    dll = a * p * (y - P) / P - (post_mean - theta) * Math.pow(sigma, -2);
+                    ddll = Math.pow(a, 2) * p * q * (guessing * y - Math.pow(P, 2)) * Math.pow(P, -2) - Math.pow(sigma, -2);
+                }
+                if (dll > 0) {
+                    low = post_mean;
+                } else if (dll < 0) {
+                    high = post_mean;
+                }
+                post_mean -= dll / ddll;
+                if (post_mean >= high || post_mean <= low) {
+                    if (high < Infinity && low > -Infinity) {
+                        post_mean = (high + low) / 2;
+                    } else if (high < Infinity) {
+                        post_mean -= delta;
+                        delta *= 2;
+                    } else {
+                        post_mean += delta;
+                        delta *= 2;
+                    }
+                }
+            } while (x !== post_mean);
+            post_sd = Math.min(Math.sqrt(-1 / ddll), max_sigma);
+            return {
+                post_mean: post_mean,
+                post_sd: post_sd
             };
-            KidaptiveIrt.estimate = function(y, beta, theta, sigma, post_mean, post_sd) {
-                return estimate(y || 0, beta || 0, theta || 0, sigma || 0, post_mean || 0, post_sd || 0);
-            };
-        })();
+        };
         return KidaptiveIrt;
     }();
     (function(global, factory) {
@@ -6190,7 +6112,7 @@
             this.sdk.modelManager.setLocalAbility(learnerId, ability);
         };
         return KidaptiveAttemptProcessor;
-    }(irt_irt, kidaptive_utils);
+    }(kidaptive_irt, kidaptive_utils);
     kidaptive_constants = function() {
         var KidaptiveConstants = {
             HOST_PROD: "https://service.kidaptive.com/v3",
